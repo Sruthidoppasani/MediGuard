cf186aee01628f0262ea63e2014454b4fa0d67f5
Sruthidoppasani <204141213+Sruthidoppasani@users.noreply.github.com>
2026-04-02 06:00:44 +0530

    Create sendUserData.js

---
+++ Agent-UI/src/api/sendUserData.js
@@ -0,0 +1,9 @@
+export async function sendUserData(data) {
+  await fetch('/api/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+}
